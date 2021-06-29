cf38d56475543e7a6a1befde3b5956247aab1ce8
Tw <wei.tan@intel.com>
2021-06-29 15:36:18 +0800

    add perfeventmap and hashmap
    Signed-off-by: Tw <wei.tan@intel.com>

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,15 +1,32 @@
 #![no_std]
 #![no_main]
 
-use aya_bpf::macros::*;
-use aya_bpf::programs::ProbeContext;
+use aya_bpf::{
+    cty::*,
+    macros::*,
+    maps::{HashMap, PerfMap},
+    programs::ProbeContext,
+    BpfContext,
+};
 
 #[panic_handler]
 fn do_panic(_info: &core::panic::PanicInfo) -> ! {
     unreachable!()
 }
 
-#[kprobe(name="kprobe")]
-fn kprobe(_ctx: ProbeContext) {
-}
+#[map]
+static mut HIT_PIDS: PerfMap<u32> = PerfMap::new(0);
 
+#[map]
+static mut COMMS: HashMap<u32, [c_char; 16]> = HashMap::with_max_entries(64, 0);
+
+#[kprobe]
+fn kprobe(ctx: ProbeContext) {
+    let pid = ctx.pid();
+    let comm = ctx.command().unwrap_or([0; 16]);
+
+    unsafe {
+        COMMS.insert(&pid, &comm, 0);
+        HIT_PIDS.output(&ctx, &pid, 0);
+    }
+}
--- user/src/main.rs
+++ user/src/main.rs
@@ -1,11 +1,40 @@
-use aya::Bpf;
+use aya::{
+    maps::{
+        hash_map::HashMap,
+        perf::{AsyncPerfEventArray, PerfBufferError},
+    },
+    programs::KProbe,
+    util::online_cpus,
+    Bpf, Btf, Pod,
+};
+use bytes::BytesMut;
+use std::{
+    convert::{TryFrom, TryInto},
+    fmt,
+    sync::Arc,
+    thread, time,
+};
+use tokio::task;
 
-fn main() -> anyhow::Result<()> {
-    let mut bpf = Bpf::load_file("../target/bpfel-unknown-none/debug/kernel").or_else(|e| {
-        println!("errno: {}", std::io::Error::last_os_error());
-        Err(e)
-    })?;
+#[derive(Copy, Clone, Debug)]
+struct Comm([u8; 16]);
 
+unsafe impl Pod for Comm {}
+
+impl fmt::Display for Comm {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let s = String::from_utf8_lossy(&self.0[..]);
+        let s = s.trim_matches(char::from(0));
+        write!(f, "{}", s)
+    }
+}
+
+#[tokio::main]
+pub async fn main() -> anyhow::Result<()> {
+    let code = include_bytes!("../../target/bpfel-unknown-none/debug/kernel").to_vec();
+    let mut bpf = Bpf::load(&code, Btf::from_sys_fs().ok())?;
+
+    // dump
     for (name, map) in bpf.maps() {
         println!(
             "found map `{}` of type `{:?}`",
@@ -22,6 +51,45 @@ fn main() -> anyhow::Result<()> {
         );
     }
 
+    // register callbacks
+    let mut perf_array = AsyncPerfEventArray::try_from(bpf.map_mut("HIT_PIDS")?)?;
+    let comms = Arc::new(HashMap::try_from(bpf.map("COMMS")?)?);
+
+    for cpu_id in online_cpus()? {
+        // open a separate perf buffer for each cpu
+        let mut buf = perf_array.open(cpu_id, None)?;
+        let db = Arc::clone(&comms);
+
+        // process each perf buffer in a separate task
+        task::spawn(async move {
+            let mut buffers = (0..10)
+                .map(|_| BytesMut::with_capacity(64))
+                .collect::<Vec<_>>();
+
+            loop {
+                // wait for events
+                let events = buf.read_events(&mut buffers).await?;
+
+                // events.read contains the number of events that have been read,
+                // and is always <= buffers.len()
+                for i in 0..events.read {
+                    let buf = &buffers[i];
+                    let pid = u32::from_le_bytes(buf[..4].try_into().unwrap());
+                    let comm: Comm = unsafe { db.get(&pid, 0).unwrap() };
+                    println!("{}: {}", pid, comm);
+                }
+            }
+
+            Ok::<_, PerfBufferError>(())
+        });
+    }
+
+    // attach program
+    let prog: &mut KProbe = bpf.program_mut("kprobe")?.try_into()?;
+    prog.load()?;
+    prog.attach("__x64_sys_openat", 0, None)?;
+
+    thread::sleep(time::Duration::from_secs(2));
 
     Ok(())
 }
